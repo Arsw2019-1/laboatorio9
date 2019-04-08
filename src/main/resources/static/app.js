31399c33bb29ff3131d11fc196558999f3eead53
unknown <2098325@labinfo.is.escuelaing.edu.co>
2019-04-08 18:03:39 -0500

    Avance

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -9,6 +9,15 @@ var app = (function () {
     
     var stompClient = null;
 
+
+        var puPoint = function(px,py){
+            var pt=new Point(px,py);
+            console.info("publishing point at "+pt);
+            addPointToCanvas(pt);
+            stompClient.send("/topic/newpoint", {}, JSON.stringify({pt}));
+            //publicar el evento
+        }
+
     var addPointToCanvas = function (point) {   
         
         var canvas = document.getElementById("canvas");
@@ -42,8 +51,8 @@ var app = (function () {
             //alert("lolo2");
             stompClient.subscribe('/topic/newpoint', function (eventbody) {
                 alert(""+eventbody);
-                var theObject=JSON.parse(eventbody.body);
-                addPointToCanvas(theObject);
+                var point=JSON.parse(eventbody.body);
+                addPointToCanvas(point);
             
             });
         });
@@ -54,9 +63,21 @@ var app = (function () {
 
     return {
 
-        init: function () {
+
+   init: function () {
             var can = document.getElementById("canvas");
             
+                canvas.addEventListener("mousedown",function (e){
+                    point=getMousePosition(e);
+
+                    addPointToCanvas(point);
+                    puPoint(point.x,point.y);
+                
+                    
+
+                })
+
+
             //websocket connection
             connectAndSubscribe();
         },
